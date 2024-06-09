c960d665f7bacdc77b7d2bb984623ede69d6a78e
Kim <kimnovak187@gmail.com>
2024-06-09 17:25:11 +0200

    [feat] Add search by name functionality

--- src/components/PokemonCatalogue/PokemonCatalogue.tsx
+++ src/components/PokemonCatalogue/PokemonCatalogue.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { Link } from '@tanstack/react-router';
 import * as api from '../../api';
+import { useSearchQuery } from '../../hooks/useSearchQuery';
+import { LoadingSpinner } from '../../ui/LoadingSpinner/LoadingSpinner';
 
 type PokemonCatalogueItem = {
   name: string;
@@ -11,6 +13,11 @@ export const PokemonCatalogue = () => {
   const [pokemons, setPokemons] = useState<PokemonCatalogueItem[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [hasError, setHasError] = useState(false);
+  const { results, updateQuery, searchQuery } =
+    useSearchQuery<PokemonCatalogueItem>({
+      dataSet: pokemons,
+      criteria: 'name',
+    });
 
   const getPokemons = async () => {
     setHasError(false);
@@ -30,11 +37,25 @@ export const PokemonCatalogue = () => {
   }, []);
 
   if (isLoading) {
-    return 'loading';
+    return (
+      <div className="max-w-md mx-auto mt-4 p-4 bg-white rounded-lg shadow-lg flex justify-center">
+        <LoadingSpinner />
+      </div>
+    );
   }
 
-  if (hasError) {
-    return 'error';
+  if (hasError || !pokemons.length) {
+    return (
+      <div className="max-w-md mx-auto mt-4 p-4 bg-white rounded-lg shadow-lg flex flex-col items-center justify-center text-center">
+        All Pokémon are currently sleeping, try again to wake them up!
+        <button
+          className="bg-primary rounded text-white px-6 py-2 flex mt-2 hover:opacity-90"
+          onClick={getPokemons}
+        >
+          RETRY
+        </button>
+      </div>
+    );
   }
 
   return (
@@ -45,18 +66,33 @@ export const PokemonCatalogue = () => {
       <div className="text-md mb-6 text-center">
         Start by selecting a Pokémon from the list
       </div>
-      <ul>
-        {pokemons.map(pokemon => (
-          <li
-            key={pokemon.name}
-            className="bg-white font-m my-2 rounded hover:opacity-70"
-          >
-            <Link to={`/pokemon/${pokemon.name}`}>
-              <div className="p-4">{pokemon.name}</div>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <div className="flex items-center border-b border-b-2 border-primary py-2 mb-4">
+        <input
+          type="search"
+          value={searchQuery}
+          onChange={e => updateQuery(e.target.value)}
+          placeholder="Search Pokémon by name"
+          className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+        />
+      </div>
+      {results.length ? (
+        <ul>
+          {results.map(pokemon => (
+            <li
+              key={pokemon.name}
+              className="bg-white font-m my-2 rounded hover:opacity-70"
+            >
+              <Link to={`/pokemon/${pokemon.name}`}>
+                <div className="p-4">{pokemon.name}</div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="text-l mb-6 text-center">
+          We couldn't find a Pokémon with that name, try with different one!
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/hooks/useSearchQuery.ts
@@ -0,0 +1,50 @@
+import { useNavigate, useSearch } from '@tanstack/react-router';
+import { useEffect, useState } from 'react';
+
+type UseSearchQueryProps<T> = {
+  dataSet: T[];
+  criteria: string;
+};
+
+/**
+ * Filters a dataset based on a search query and specified criteria.
+ * Updates the URL search query to reflect the current filter state.
+ * @param options - The options object.
+ * @param options.dataset - The dataset to search through.
+ * @param options.criteria - The property of the dataset to filter by.
+ * @returns Object containing the filtered results, search query and a function to update the search query.
+ */
+export const useSearchQuery = <T extends Record<string, any>>({
+  dataSet,
+  criteria,
+}: UseSearchQueryProps<T>) => {
+  const search: Record<string, string> = useSearch({
+    from: '/',
+  });
+  const searchQuery = search[criteria] || '';
+  const navigate = useNavigate();
+  const [results, setResults] = useState<T[]>(dataSet);
+
+  useEffect(() => {
+    const filteredItems = dataSet.filter(item =>
+      item[criteria].includes(searchQuery)
+    );
+    setResults(filteredItems);
+  }, [search, dataSet]);
+
+  const updateURLSearchParams = (newQuery: string = '') => {
+    navigate({
+      search: (prev: T) => ({
+        ...prev,
+        [criteria]: newQuery,
+      }),
+      replace: !!searchQuery,
+    });
+  };
+
+  const updateQuery = (newQuery: string = '') => {
+    updateURLSearchParams(newQuery);
+  };
+
+  return { results, searchQuery, updateQuery };
+};
